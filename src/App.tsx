68f13b6dec45a5f481a21236a6b12c7c3a601030
반달곰수 <121839239+bandalgomsu@users.noreply.github.com>
2025-05-02 14:01:44 +0900

    refactor : 기본 스케일 크기 변경

--- src/App.tsx
+++ src/App.tsx
@@ -128,7 +128,7 @@ function App() {
 
     return (
         <div className="bg-white text-gray-900 flex flex-col min-h-screen justify-center items-center px-4
-                scale-90 sm:scale-100 transition-transform origin-top">
+                scale-105 sm:scale-110 transition-transform origin-top">
             {/* 로고 */}
             <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold mb-8 text-blue-600 text-center">
                 CS <span className="text-blue-500">Forever</span>
